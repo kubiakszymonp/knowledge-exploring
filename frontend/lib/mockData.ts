f0edd651cd8ff894fa6378e9f4c01cfdc077fee2
Szymon Kubiak <kubiakszymonp@gmail.com>
2025-12-28 00:02:51 +0100

    fix

--- frontend/lib/mockData.ts
+++ frontend/lib/mockData.ts
@@ -1,7 +1,8 @@
 import { KnowledgeGraph } from "../model/KnowledgeGraph";
 import { mapJSONToGraph } from "./graphMapper";
 import graphData from "../data/brama_florianska.json";
+import type { GraphJSON } from "./graphMapper";
 
 export function createMockGraph(): KnowledgeGraph {
-  return mapJSONToGraph(graphData);
+  return mapJSONToGraph(graphData as GraphJSON);
 }
